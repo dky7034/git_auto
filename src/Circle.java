495fa1a11132e12b46ff96eb26fc1caf7fd7f9cc
artp <dky7034@naver.com>
2025-02-27 19:01:27 +0900

    Auto commit: src\Circle.java

--- src/Circle.java
+++ src/Circle.java
@@ -2,5 +2,6 @@ public class Circle {
     public static void main(String[] args) {
         System.out.println("circle");
         System.out.println("circlecircle");
+        System.out.println("circlecircle");
     }
 }
